8320b7be49ff6052128810afceefa820d9d7d386
Thuan, Daniel (HQP) <thithu01@roberthalf.com>
2019-10-22 10:43:06 -0700

    Add clean number string service to seperate number from delimiters

---
+++ StringCalculatorMainProgram/Services/CleanedNumberStringService.cs
@@ -0,0 +1,18 @@
+﻿using StringCalculatorMainProgram.ServiceInterfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringCalculatorMainProgram.Services
+{
+    public class CleanedNumberStringService : ICleanedNumberStringService
+    {
+        private string _numbers;
+        private string _delimiters;
+
+        public object CleanNumbersString()
+        {
+            return new { _numbers, _delimiters };
+        }
+    }
+}
