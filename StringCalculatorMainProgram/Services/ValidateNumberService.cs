31aaa1c2bbf2001dcd0091c8a39f8bb549e46bbc
Thuan, Daniel (HQP) <thithu01@roberthalf.com>
2019-10-22 15:05:34 -0700

    clean up code

--- StringCalculatorMainProgram/Services/ValidateNumberService.cs
+++ StringCalculatorMainProgram/Services/ValidateNumberService.cs
@@ -17,9 +17,7 @@ namespace StringCalculatorMainProgram.Services
             return ValidatorStatus.NormalNumber;
         }
         private bool IsNegative(IDetectNegativeNumbersService detectNegativeNumbersService, IDetectDelimitersService detectDelimitersService, string numbers, string delimiters) 
-        {
-            return detectNegativeNumbersService.isNegativeNumbersExistInString(detectDelimitersService, numbers, delimiters) ? true : false;
-        }
+            => detectNegativeNumbersService.isNegativeNumbersExistInString(detectDelimitersService, numbers, delimiters) ? true : false;
         private bool CheckNoNumbersExist(string numbers) => String.IsNullOrWhiteSpace(numbers);
         private bool CheckIfOnlyContainOneNumber(string numbers) => numbers.Length == 1 && int.Parse(numbers) > 0;
     }
