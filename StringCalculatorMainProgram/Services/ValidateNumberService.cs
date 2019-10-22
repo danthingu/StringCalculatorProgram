64ab3c54b9112f86683a5eda577b6ec8ea01070f
Thuan, Daniel (HQP) <thithu01@roberthalf.com>
2019-10-22 10:47:52 -0700

    Check if string only contain 1 number

--- StringCalculatorMainProgram/Services/ValidateNumberService.cs
+++ StringCalculatorMainProgram/Services/ValidateNumberService.cs
@@ -11,11 +11,12 @@ namespace StringCalculatorMainProgram.Services
         public ValidatorStatus ValidateNumber(string numbers, string delimiter = null)
         {
             if (CheckNoNumbersExist(numbers)) return ValidatorStatus.NoNumbers;
+            if (CheckIfOnlyContainOneNumber(numbers)) return ValidatorStatus.SingleNumber;
 
             return ValidatorStatus.NormalNumber;
         }
 
         private bool CheckNoNumbersExist(string numbers) => String.IsNullOrWhiteSpace(numbers);
-        
+        private bool CheckIfOnlyContainOneNumber(string numbers) => int.Parse(numbers) > 0 && numbers.Length == 1;
     }
 }
