807ffa798b34006f93c55e6e94d72ec5468d3b6d
Thuan, Daniel (HQP) <thithu01@roberthalf.com>
2019-10-22 10:23:33 -0700

    First Commit, created all the empty projects

---
+++ StringCalculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using StringCalculatorMainProgram;
+using System;
+
+namespace StringCalculator
+{
+    class Program
+    {
+        //The Inversion of Control(IoC) and Dependency Injection(DI) patterns are all about removing dependencies from your code.
+        static void Main(string[] args) =>
+            _ = new StringCalculatorMainApplication();
+    }
+}
---
+++ StringCalculatorMainProgram/StringCalculatorMainApplication.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StringCalculatorMainProgram
+{
+    public class StringCalculatorMainApplication
+    {
+        public StringCalculatorMainApplication()
+        {
+
+        }
+    }
+}
